3b2623b5d795e5c5c15068dd48ee085bb349acaf
Lila Marie Marie <120058995+lilamgb@users.noreply.github.com>
2022-12-07 22:25:30 +0000

    paso 4

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-with open('gift_costs.txt') as f:
+"""
+Lila Guzman
+22-0638
+Refinando código
+aqui, se refinará un código dado y se publicará en un repositorio de Github
+"""
+
+
+def costos_lista():
+  with open('gift_costs.txt') as f:
     gift_costs = f.read().split('\n')
-    
-gift_costs = [int(c) for c in gift_costs]  # convierte strings a int
+  gift_costs = [int(c) for c in gift_costs]  # convierte strings a int
+  return gift_costs
 
-total_price = 0
-for cost in gift_costs:
+
+def total(gift_costs):
+  gift_costs=costos_lista()
+  total_price = 0
+  for cost in gift_costs:
     if cost > 1000:
-        total_price += cost * 1.16  # agrega impuestos
+      total_price += cost * 1.16  # agrega impuestos
     else:
-        total_price += cost
+      total_price += cost  # los costos menores a 1000 no se le agrega impuesto
 
-print(total_price)
+  return total_price
+
+# llama a los dos funciones y luego imprime el resultado
+def main():
+  print(total(costos_lista))
+        
+if __name__ == '__main__':
+  main()
